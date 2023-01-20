e0fc6eba0b3bd03d58399a98190bea94e8abb8bc
Shikhar Mishra <shikharmishra730@gmail.com>
2023-01-20 23:09:02 +0530

    init

---
+++ bot.py
@@ -0,0 +1,53 @@
+import os
+import re
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from greeting import get_greeting
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has connected to Discord!")
+
+
+@bot.command()
+async def meme(ctx):
+    await ctx.send("meem")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    messageContent = message.content.strip().lower()
+    # print(message.author, message.content, message.channel.id)
+    if messageContent == "hello there":
+        await message.channel.send("General Kenobi")
+    elif bool(re.search(r"\b(hel+o+|hey+|hi+)\b", messageContent)):
+        await message.channel.send(get_greeting(message.author))
+    await bot.process_commands(message)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("You're missing a required argument.¯\_(ツ)_/¯")
+        return
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("That's not a valid command!")
+        return
+    await ctx.send("Couldn't run the command. (> <)")
+
+
+bot.run(TOKEN)
