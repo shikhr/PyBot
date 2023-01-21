ab506fd0f408022429ccd2f9a12d511769c355d0
Shikhar Mishra <shikharmishra730@gmail.com>
2023-01-21 16:14:14 +0530

    reformat memegen wrapper

--- create_meme.py
+++ create_meme.py
@@ -1,22 +1,33 @@
-replments = {
+replments_params = {
     "_": "__",
     " ": "_",
     "-": "--",
     "?": "~q",
     "&": "~a",
+    "%": "~p",
+    "#": "~h",
     "/": "~s",
     "\\": "~b",
-    "#": "~h",
-    '""': "''",
+    "<": "~l",
+    ">": "~g",
+    '"': "''",
 }
+replments_key = replments_params.copy()
+del replments_key["-"]
+
+allowed_ext = ["png", "gif", "jpg", "webp"]
 
 
 def gen_meme_url(args):
     if len(args) == 0:
         return "No arguments supplied! Use .mkmem --help to see how to use the command."
-    params = []
-    for word in args:
-        params.append("".join([replments.get(c, c) for c in word]))
-    params[0] = params[0].replace("--", "-")
-    gen_url = "/".join(params[:-1]) + f".{params[-1]}"
+    generator = {"key": "", "params": [], "ext": "png"}
+    generator["key"] = "".join([replments_key.get(c, c) for c in args[0]])
+    if args[-1] in allowed_ext:
+        generator["ext"] = args[-1].lower()
+    for word in args[1:-1]:
+        generator["params"].append("".join([replments_params.get(c, c) for c in word]))
+    gen_url = (
+        "/".join([generator["key"]] + generator["params"]) + f'.{generator["ext"]}'
+    )
     return f"please wait while the meme is generating:\n https://api.memegen.link/images/{gen_url}"
